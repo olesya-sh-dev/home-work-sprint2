6d83558f0f03598b74042640b8aa648c7d06d4c4
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-04-28 23:35:04 +0300

    HW7 select, label, radio-button

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,6 +35,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChangeOption!(+e.currentTarget.value)
         // делают студенты
     }
 
@@ -48,7 +49,11 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      // name, checked, value делают студенты
+                      name={name}
+                      value={o.id}
+                      checked={value === o.id}
+                             
+                     // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
                       {...restProps}
