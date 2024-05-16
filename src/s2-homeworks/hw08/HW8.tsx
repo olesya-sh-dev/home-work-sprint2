fadfdb6590a13adb6bd01535cdcc411c5ec62abf
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-05-16 09:39:43 +0300

    HW8 sort

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -84,8 +84,8 @@ const HW8 = () => {
 
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
-                        <tr>
-                            <td className={s.nameCol}>Name</td>
+                        <tr className={s.item}>
+                            <td className={s.nameCol}>Full name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
                         </thead>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,32 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+       const newState = [...state].sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
         }
-        case 'check': {
-
-            return state // need to fix
+        if (a.name < b.name) {
+          return -1;
         }
-        default:
-            return state
+        return 0;
+      });
+      return action.payload === "up" ? newState : newState.reverse();
     }
-}
+     
+    case "check": {
+      return state.filter((u) => u.age >= action.payload); // need to fix
+    }
+    default:
+      return state;
+  }
+};
